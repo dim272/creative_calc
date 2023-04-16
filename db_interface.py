5861ecab12e8addb9b0178b733f069adc459e49b
dim_g <fewmoments@yahoo.com>
2023-04-16 19:52:39 +0300

    db updae, calculating func added

--- db_interface.py
+++ db_interface.py
@@ -2,9 +2,10 @@
 Класс для общения с базой данных
 """
 import sqlite3
+from datetime import datetime
 
 
-class DatabaseInterface:
+class DbConnection:
     """
     В классе реализованы функции чтения и добавления данных в базу
     """
@@ -21,11 +22,11 @@ class DatabaseInterface:
                 CREATE TABLE IF NOT EXISTS user 
                 (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    email TEXT UNIQUE,
-                    password TEXT,
+                    email TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL,
                     name TEXT,
                     about TEXT,
-                    added DATETIME
+                    date_added DATETIME NOT NULL
                 )
                 """
             )
@@ -34,9 +35,10 @@ class DatabaseInterface:
                 CREATE TABLE IF NOT EXISTS handmade 
                 (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT,
-                    added DATETIME,
-                    user_id INTEGER
+                    name TEXT NOT NULL,
+                    date_added DATETIME NOT NULL,
+                    is_private INTEGER NOT NULL,
+                    user_id INTEGER NOT NULL,
                     FOREIGN KEY (user_id) REFERENCES user (id) 
                 );
                 """
@@ -46,36 +48,127 @@ class DatabaseInterface:
                 CREATE TABLE IF NOT EXISTS material 
                 (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT,
-                    quantity INTEGER,
-                    total_quantity INTEGER,
-                    price INTEGER,
-                    handmade_id INTEGER
+                    name TEXT NOT NULL,
+                    quantity INTEGER NOT NULL,
+                    total_quantity INTEGER NOT NULL,
+                    price INTEGER NOT NULL,
+                    handmade_id INTEGER NOT NULL,
                     FOREIGN KEY (handmade_id) REFERENCES handmade (id) 
                 )
                 """
             )
             self.con.commit()
 
-    def read_user(self):
-        pass
+    def save_user(self, **kwargs):
+        with self.con:
+            cur = self.con.cursor()
+            date_added = datetime.now()
+            cur.execute(
+                """
+                INSERT INTO user (email, password, name, about, date_added)
+                VALUES (?, ?, ?, ?, ?)
+                """,
+                (kwargs.get('email'), kwargs.get('password'), kwargs.get('name'), kwargs.get('about'), date_added)
+            )
+            self.con.commit()
 
-    def read_handmade(self):
-        pass
+    def save_handmade(self, **kwargs):
+        with self.con:
+            cur = self.con.cursor()
+            date_added = datetime.now()
+            cur.execute(
+                """
+                INSERT INTO handmade (name, date_added, user_id, is_private)
+                VALUES (?, ?, ?, ?)
+                """,
+                (kwargs.get('name'), date_added, kwargs.get('user_id'), kwargs.get('is_private'))
+            )
+            self.con.commit()
 
-    def read_material(self):
-        pass
+    def save_materials(self, materials_list):
+        with self.con:
+            cur = self.con.cursor()
+            for materials in materials_list:
+                cur.execute(
+                    """
+                    INSERT INTO material (name, quantity, total_quantity, price, handmade_id)
+                    VALUES (?, ?, ?, ?, ?)
+                    """,
+                    materials
+                )
+            self.con.commit()
 
-    def write_value(self):
-        self.cur.execute(
-            """
-            INSERT INTO user (email, password, name, about, added) 
-            VALUES ('first_user@mail.ru', 'test123', 'Helen', 'Люблю кофе и котиков', '03.04.2023')
-            """
-        )
-        self.con.commit()
+    def get_total_handmade_list(self, limit):
+        with self.con:
+            cur = self.con.cursor()
+            cur.execute(
+                """
+                SELECT * FROM handmade
+                ORDER BY date_added
+                LIMIT (?)
+                """,
+                (limit,)
+            )
+            handmade_list = cur.fetchall()
+        return handmade_list
+
+    def get_user_handmade_list(self, user_id):
+        with self.con:
+            cur = self.con.cursor()
+            cur.execute(
+                """
+                SELECT * FROM handmade
+                WHERE user_id = (?)
+                ORDER BY date_added
+                """,
+                (user_id,)
+            )
+            handmade_list = cur.fetchall()
+        return handmade_list
+
+    def get_material_list(self, handmade_id):
+        with self.con:
+            cur = self.con.cursor()
+            cur.execute(
+                """
+                SELECT * FROM material
+                WHERE handmade_id = (?)
+                ORDER BY id
+                """,
+                (handmade_id,)
+            )
+            material_list = cur.fetchall()
+        return material_list
 
 
 if __name__ == '__main__':
-    x = DatabaseInterface()
-    x.write_value()
+    x = DbConnection()
+    # x.save_user(
+    #     **{
+    #         'email': 'test@test.com',
+    #         'password': 'test_pass',
+    #         'name': 'test_name',
+    #         'about': 'test test test'
+    #     }
+    # )
+    # x.save_handmade(
+    #     **{
+    #         'name': 'Свеча',
+    #         'user_id': 1,
+    #         'is_private': 0
+    #     }
+    # )
+    # x.save_materials(
+    #     [
+    #         ('Воск', 100, 500, 349, 1),
+    #         ('Фитиль', 1, 20, 149, 1),
+    #         ('Отдушка', 10, 100, 499, 1)
+    #     ]
+    # )
+    m = x.get_material_list(1)
+    print(m)
+
+    # from main import calculating
+    # price = calculating(m)
+    # print(price)
+
