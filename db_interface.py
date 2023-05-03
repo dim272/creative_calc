32469ea62aa1df329a621013e30d85236d203870
Your Name <you@example.com>
2023-05-03 14:08:09 +0300

    db-интерфейс

--- db_interface.py
+++ db_interface.py
@@ -1,67 +1,142 @@
-"""
-Класс для общения с базой данных
-"""
 import sqlite3
+from datetime import datetime
 
 
-class DatabaseInterface:
-    """
-    В классе реализованы функции чтения и добавления данных в базу
-    """
-
+class DataBase:
     def __init__(self):
-        self.con = sqlite3.connect('cc_demo.db')
-        self.cur = self.con.cursor()
-        self._init_tables()
+        self.connect = sqlite3.connect('lavander.db')
+        self.create_db()
 
-    def _init_tables(self):
-        self.cur.execute(
-            """
-            CREATE TABLE IF NOT EXISTS user 
-            (
-                id INTEGER PRIMARY KEY,
-                email TEXT,
-                password TEXT,
-                name TEXT,
-                about TEXT,
-                added DATETIME
+    def create_db(self):
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS user 
+                (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    email TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL,
+                    name TEXT NOT NULL,
+                    date_added DATETIME NOT NULL
+                ) 
+                """
             )
-            """
-        )
 
-        self.cur.execute(
-            """
-            CREATE TABLE IF NOT EXISTS handmade 
-            (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                added DATETIME,
-                user_id INTEGER
-            );
-            """
-        )
-
-        self.cur.execute(
-            """
-            CREATE TABLE IF NOT EXISTS material 
-            (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                quantity INTEGER,
-                price INTEGER,
-                handmade_id INTEGER
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS handmade
+                (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    is_private INTEGER NOT NULL,
+                    user_id INTEGER NOT NULL,
+                    price INTEGER NOT NULL,
+                    date_added DATETIME NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES user (id)
+                ) 
+                """
             )
-            """
-        )
 
-    def read_user(self):
-        pass
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS material
+                (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    quantity INTEGER NOT NULL,
+                    total_quantity INTEGER NOT NULL,
+                    price INTEGER NOT NULL,
+                    handmade_id INTEGER NOT NULL,
+                    date_added DATETIME NOT NULL,
+                    FOREIGN KEY (handmade_id) REFERENCES handmade (id)
+                ) 
+                """
+            )
+            self.connect.commit()
 
-    def read_handmade(self):
-        pass
+    def create_user(self, email, password, name):
+        with self.connect:
+            date_added = datetime.now()
 
-    def read_material(self):
-        pass
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                INSERT INTO user (email,password,name,date_added)
+                VALUES (?,?,?,?)
+                """,
+                (email, password, name, date_added)
+            )
+            self.connect.commit()
 
-    def write_value(self):
-        pass
+    def create_handmade(self, name, is_private, user_id,price):
+        with self.connect:
+            date_added = datetime.now()
+
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                INSERT INTO handmade (name,is_private,date_added,user_id,price)
+                VALUES (?,?,?,?,?)
+                """,
+                (name, is_private, date_added, user_id, price)
+
+            )
+            self.connect.commit()
+
+    def create_materials(self, handmade_id, materials):
+        with self.connect:
+            date_added = datetime.now()
+            cursor = self.connect.cursor()
+            for material in materials:
+                cursor.execute(
+                    """
+                    INSERT INTO material (name,quantity,total_quantity,price,handmade_id,date_added)
+                    VALUES (?,?,?,?,?,?)
+                    """,
+                    (material['name'], material['quantity'], material['total_quantity'], material['price'], handmade_id,
+                     date_added)
+                )
+            self.connect.commit()
+
+    def get_last_handmade(self, limit=25):
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                SELECT * FROM handmade 
+                WHERE is_private = 0
+                ORDER BY id DESC 
+                LIMIT (?)               
+                """,
+                (limit,)
+            )
+            handmade_list=cursor.fetchall()
+        return handmade_list
+
+    def get_handemade_by_user(self,user_id):
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                SELECT * FROM handmade
+                WHERE user_id = (?)
+                ORDER BY id DESC
+                """,
+                (user_id,)
+            )
+            handmade_list = cursor.fetchall()
+            return handmade_list
+
+    def get_materials(self,handmade_id):
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                SELECT * FROM material
+                WHERE handmade_id = (?)
+                """,
+                (handmade_id,)
+            )
+            materials = cursor.fetchall()
+            return materials
