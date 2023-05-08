96bade912c3b71858e98156bece61061a984915d
dim_g <fewmoments@yahoo.com>
2023-05-08 18:18:48 +0300

    registration and authorization added

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 from db_interface import DataBase
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 
 @app.route("/")
 def main_page():
-    db = DataBase()
     handmade_items = []
+    db = DataBase()
     handmade_list = db.get_last_handmade(limit=20)
     for item in handmade_list:
         handmade_items.append({
@@ -18,12 +19,52 @@ def main_page():
             'price': item[2],
             'user_name': item[3]
         })
-    return render_template("index.html", handmade_list=handmade_items)
+    return render_template("index.html", h1='Лучшие работы', handmade_list=handmade_items)
 
 
 @app.route("/login.html")
 def login_page():
-    return "<h1>Creative calc</h1>"
+    return render_template("login.html", h1='Авторизация')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    db = DataBase()
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user_info = db.login(email, password)
+    if user_info:
+        user_id = user_info[0]
+        user_name = user_info[3]
+
+        session['loggedin'] = True
+        session['id'] = user_id
+        session['username'] = user_name
+
+        msg = f"Здравствуйте {user_name}"
+    else:
+        msg = "Вы ввели неправильный логин или пароль."
+    return render_template("login.html", msg=msg)
+
+
+@app.route("/registration.html")
+def registration_page():
+    return render_template("registration.html", h1='Регистрация')
+
+
+@app.route('/registration', methods=['POST'])
+def registration():
+    db = DataBase()
+    email = request.form.get('email')
+    name = request.form.get('username')
+    password = request.form.get('password')
+    user_exist = db.check_user_exists(email)
+    if user_exist:
+        msg = "Пользователь с таким email уже зарегистрирован."
+    else:
+        db.create_user(email, password, name)
+        msg = "Регистрация прошла успешно."
+    return render_template("registration.html", msg=msg)
 
 
 if __name__ == '__main__':
--- db_interface.py
+++ db_interface.py
@@ -185,6 +185,49 @@ class DataBase:
             materials = cursor.fetchall()
         return materials
 
+    def check_user_exists(self, email: str) -> bool:
+        """Производит поиск в таблице user на наличие строки с переданными значением email.
+
+        Используется в процессе регистрации и авторизации.
+
+        :param email: Адрес электронной почты пользователя.
+        :return: Существует / не существует пользователь с таким email.
+        """
+
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                SELECT * FROM user
+                WHERE email = ?
+                """,
+                (email,)
+            )
+            return bool(cursor.fetchone())
+
+    def login(self, email: str, password: str) -> Tuple[str]:
+        """Производит поиск в таблице user на наличие строки с переданными значениями email и password.
+
+        Используется при авторизации пользователя.
+        Возвращает True, если пользователь введёт правильные email и password, находящиеся в бд.
+        Иначе возвращает False.
+
+        :param email: Адрес электронной почты пользователя.
+        :param password: Пароль пользователя.
+        :return: Данные пользователя с указанным email и password.
+        """
+
+        with self.connect:
+            cursor = self.connect.cursor()
+            cursor.execute(
+                """
+                SELECT * FROM user
+                WHERE email = ? AND password = ?
+                """,
+                (email, password)
+            )
+            return cursor.fetchone()
+
 
 if __name__ == "__main__":
     db = DataBase()
